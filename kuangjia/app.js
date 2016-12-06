48a5669cd1737f18a66a053204aef4bb98a1f1fb
ZichengYan <464554492@qq.com>
2016-12-06 10:12:52 +0800

    跑通前后端

---
+++ kuangjia/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var poster = require('http-poster');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+// app.use('/users', users);
+
+app.get('/re', function(req, res) {
+        poster.get('https://www.baidu.com', function(err, result, data) {
+        	console.log(err);
+            console.log(result);
+            console.log(data);
+            res.end(data)
+        });
+})
+module.exports = app;
---
+++ nokuangjia/index3000.js
@@ -0,0 +1,7 @@
+var http=require("http")
+var server=http.createServer()
+
+server.on("request",function (req,res) {
+	// 在这里向另一个服务器发送请求,如何写代码
+}).listen(3000);
+

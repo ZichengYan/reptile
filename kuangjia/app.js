c9c8f0672fff53e087fb69b93f28e8fb7a56804d
ZichengYan <464554492@qq.com>
2016-12-06 14:03:54 +0800

    版本初成待完善

--- kuangjia/app.js
+++ kuangjia/app.js
@@ -1,46 +1,28 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
-var poster = require('http-poster');
 var fs = require('fs');
-var http=require('http');
-var request=require('request');
+var request = require('request');
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
-
 var app = express();
-// app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 // app.use('/', routes);
 // app.use('/users', users);
 //http://r.qidian.com/
 app.get('/re', function(req, res) {
-	/*var html=""
-    poster.get('https://www.baidu.com', function(err, result, data) {
-        // console.log(err);
-        // console.log(result);
-        html+=data;
-        console.log("=============================================================================================");
-        console.log("数据开头===========================================================================================");
-        console.log(data+"回来的数据完整吗");
-        console.log("数据结尾==========================================================================================");
-        console.log("=============================================================================================");
-        fs.writeFile('./message.txt', data, function(err) {
-            if (err) throw err;
-            console.log('It\'s saved!');
-        });
-        res.end(html+"回来的数据完整吗?")
-    });*/
-   request('https://www.baidu.com', function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-       console.log(typeof body) // Show the HTML for the Google homepage. 
-       res.end(body)
-     }
-   })
+    request('https://www.baidu.com', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(typeof body) // Show the HTML for the homepage. 
+            fs.writeFile('./testMessage.md', body, function(err) {
+                if (err) throw err;
+                console.log('It\'s saved!');
+            });
+            console.log(response)
+            res.end(body)
+        }
+    })
 })
+
 module.exports = app;

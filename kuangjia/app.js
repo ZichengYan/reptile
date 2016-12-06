65e625755195b3002f85297c8ce1ba830ce2321f
ZichengYan <464554492@qq.com>
2016-12-06 12:20:54 +0800

    服务器端使用poster发送请求

--- kuangjia/app.js
+++ kuangjia/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var poster = require('http-poster');
+var fs = require('fs');
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
 
@@ -16,11 +17,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', users);
 
 app.get('/re', function(req, res) {
-        poster.get('https://www.baidu.com', function(err, result, data) {
-        	console.log(err);
-            console.log(result);
-            console.log(data);
-            res.end(data)
+    poster.get('https://www.baidu.com', function(err, result, data) {
+        console.log(err);
+        console.log(result);
+        console.log(data);
+        fs.writeFile('./message.txt', data, function(err) {
+            if (err) throw err;
+            console.log('It\'s saved!');
         });
+        res.end(data)
+    });
 })
 module.exports = app;
